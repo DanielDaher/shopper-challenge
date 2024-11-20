6570fde9a43bd782b8167e845eed6f5d7c1ce082
Daniel <danielfdoficial@gmail.com>
2024-11-20 15:13:04 -0300

    build: add driver model/seed

--- backend/prisma/seeds/index.ts
+++ backend/prisma/seeds/index.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 import { seedAdmin } from './admin';
 import { seedUser } from './user';
+import { seedDrivers } from './driver';
 
 const prisma = new PrismaClient();
 async function main() {
   const admins = await prisma.admin.count();
   const users = await prisma.user.count();
+  const drivers = await prisma.driver.count();
 
   if ( admins === 0 ) {
     await seedAdmin(prisma);
@@ -13,6 +15,10 @@ async function main() {
 
   if ( users === 0 ) {
     await seedUser(prisma);
+  }
+
+  if ( drivers === 0 ) {
+    await seedDrivers(prisma);
   }
 }
 
