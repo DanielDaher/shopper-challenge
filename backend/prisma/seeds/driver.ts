b42cf1c08c455f67c5b5e931e52a0cf986a556f2
Daniel <danielfdoficial@gmail.com>
2024-11-23 18:24:25 -0300

    build: add review model and seed

--- backend/prisma/seeds/driver.ts
+++ backend/prisma/seeds/driver.ts
@@ -1,29 +1,46 @@
-import { Prisma, PrismaClient, AccountRole, AccountStatus, UserType } from '@prisma/client';
-import PasswordHelper from '../../src/utils/helpers/password.helper';
+import { Prisma, PrismaClient } from '@prisma/client';
 const drivers: Prisma.DriverCreateInput[] = [
   {
     name: 'Homer Simpson',
     description: 'Olá! Sou o Homer, seu motorista camarada! Relaxe e aproveite o passeio, com direito a rosquinhas e boas risadas (e talvez alguns desvios).',
-    car: 'Plymouth Valiant 1973 rosa e enferrujado',
+    vehicle: 'Plymouth Valiant 1973 rosa e enferrujado',
     minKm: 1,
-    review: '2/5. Motorista simpático, mas errou o caminho 3 vezes. O carro cheira a donuts.',
-    tax: 2.50,
+    reviews: {
+      create: {
+        customerId: 1,
+        rating: 2,
+        comment: 'Motorista simpático, mas errou o caminho 3 vezes. O carro cheira a donuts.',
+      },
+    },
+    value: 2.50,
   },
   {
     name: 'Dominic Toretto',
     description: 'Ei, aqui é o Dom. Pode entrar, vou te levar com segurança e rapidez ao seu destino. Só não mexa  o rádio, a playlist é sagrada.',
-    car: 'Dodge Charger R/T 1970 modificado',
+    vehicle: 'Dodge Charger R/T 1970 modificado',
     minKm: 5,
-    review: '4/5. Que viagem incrível! O carro é um show à parte e o motorista, apesar de ter uma cara de poucos amigos, foi super gente boa. Recomendo!',
-    tax: 5.00,
+    reviews: {
+      create: {
+        customerId: 1,
+        rating: 4,
+        comment: 'Que viagem incrível! O carro é um show à parte e o motorista, apesar de ter uma cara de poucos amigos, foi super gente boa. Recomendo!',
+      },
+    },
+    value: 5.00,
   },
   {
     name: 'James Bond',
     description: 'Boa noite, sou James Bond. À seu dispor para um passeio suave e discreto. Aperte o cinto e aproveite a viagem.',
-    car: 'Aston Martin DB5 clássico',
+    vehicle: 'Aston Martin DB5 clássico',
     minKm: 10,
-    review: '5/5. Serviço impecável! O motorista é a própria definição de classe e o carro é simplesmente magnífico. Uma experiência digna de um agente secreto.',
-    tax: 10.00,
+    reviews: {
+      create: {
+        customerId: 1,
+        rating: 5,
+        comment: 'Serviço impecável! O motorista é a própria definição de classe e o carro é simplesmente magnífico. Uma experiência digna de um agente secreto.',
+      },
+    },
+    value: 10.00,
   },
 ];
 
