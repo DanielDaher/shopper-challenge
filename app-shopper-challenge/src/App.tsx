1777bafe3bf8892ad38d11c0ff9218b70ee3e077
Daniel <danielfdoficial@gmail.com>
2024-11-26 14:40:22 -0300

    build: init frontend react ts app

---
+++ app-shopper-challenge/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import RideSolicitation from './Pages/ride-solicitation/RideSolicitation';
+
+function App() {
+  return (
+    <div className="App">
+      <RideSolicitation />
+    </div>
+  );
+}
+
+export default App;
