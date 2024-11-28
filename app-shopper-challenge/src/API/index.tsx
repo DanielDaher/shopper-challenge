7d4aaa0a644f0cbdcd7d3d742b77b1b1d0cb0e0f
Daniel <danielfdoficial@gmail.com>
2024-11-27 23:51:53 -0300

    build: use sweet alert to render errors on screen

--- app-shopper-challenge/src/API/index.tsx
+++ app-shopper-challenge/src/API/index.tsx
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import Swal from 'sweetalert2';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/';
 
@@ -11,10 +12,25 @@ api.interceptors.response.use(
   (response: AxiosResponse) => response,
   (error) => {
     if (error.response) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro na solicitação',
+        text: error.response.data?.error_description || 'Ocorreu um erro inesperado.',
+      });
       console.error('Erro de resposta:', error.response.data);
     } else if (error.request) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro na Conexão',
+        text: 'Não foi possível conectar ao servidor. Verifique sua internet.',
+      });
       console.error('Erro na requisição:', error.request);
     } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro Desconhecido',
+        text: error.message,
+      });
       console.error('Erro desconhecido:', error.message);
     }
     return Promise.reject(error);
